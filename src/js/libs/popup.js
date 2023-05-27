9b712360b5d4cb25e4d27e7d2c20abf49101d916
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2023-05-27 21:15:56 +0300

    home

--- src/js/libs/popup.js
+++ src/js/libs/popup.js
@@ -171,7 +171,9 @@ class Popup {
 	open(selectorValue) {
 		if (bodyLockStatus) {
 			// Якщо перед відкриттям попапа був режим lock
-			this.bodyLock = document.documentElement.classList.contains('lock') && !this.isOpen ? true : false;
+			// this.bodyLock = document.documentElement.classList.contains('lock') && !this.isOpen ? true : false;
+			this.bodyLock = document.documentElement.classList.contains('lock') ? true : false;
+
 
 			// Якщо ввести значення селектора (селектор настроюється в options)
 			if (selectorValue && typeof (selectorValue) === "string" && selectorValue.trim() !== "") {
